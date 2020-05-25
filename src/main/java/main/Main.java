fcc0c2792de2df8728638570de3bb2a5422e5309
Franz Nico <fnotestado@gmail.com>
2020-05-25 20:25:42 +0800

    Updated Main class

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,10 +7,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static Stage primaryStage;
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/SalesView.fxml"));
         Scene scene = new Scene(root);
 
+        Main.primaryStage = stage;
         stage.setScene(scene);
         stage.setResizable(false);
         stage.setTitle("Retailing System");
